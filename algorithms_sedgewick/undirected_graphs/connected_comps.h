3e36380c21dd966ee002da9a8f878491009f5834
Stefan Marian Popa <spopa@Unknown-6c-40-08-af-3e-da.home>
2015-12-14 10:00:19 +0000

    Undirected Graphs

--- algorithms_sedgewick/undirected_graphs/connected_comps.h
+++ algorithms_sedgewick/undirected_graphs/connected_comps.h
@@ -7,10 +7,9 @@ struct connected_comps_t{
     //finds the list of connected components
     connected_comps_t(graph_t const& g) : g{g}{
         assert(g.is_valid());
-        marked.resize(g.vertices(), false);
         cc.resize(g.vertices(), infinity);
         for(uint64_t v=0; v<g.vertices(); ++v)
-            if(!marked[v]){ dfs(v); ncc++; }
+            if(cc[v] == infinity){ dfs(v); ncc++; }
     }
 
     //is v connected to w?
@@ -32,14 +31,13 @@ struct connected_comps_t{
 
 private:
     void dfs(uint64_t v){
-        marked[v] = true; cc[v] = ncc;
+        cc[v] = ncc;
         for(auto w : g.adj(v))
-            if(!marked[w]) dfs(w);
+            if(cc[w] == infinity) dfs(w);
     }
 
     graph_t const& g;
 
-    std::vector<bool> marked;
     std::vector<uint64_t> cc;
     uint64_t ncc{0};
 };
